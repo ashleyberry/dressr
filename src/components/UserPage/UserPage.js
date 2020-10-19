a522b68c7bf7dd981c4871b4f8de79c6e96fa7f0
Ashley Berry <ashleywberry@gmail.com>
2020-10-19 10:22:36 -0500

    moved searchbar to component with message for no matching items

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -3,10 +3,11 @@ import { connect } from 'react-redux';
 // import LogOutButton from '../LogOutButton/LogOutButton';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import { withRouter } from 'react-router-dom';
+import Searchish from '../SearchBar/SearchBar';
+import './UserPage.css';
 
 
 // import Search from '../Search/Search';
-import SearchBar from "material-ui-search-bar";
 
 import LibraryAddIcon from '@material-ui/icons/LibraryAdd';
 import { 
@@ -50,57 +51,24 @@ class UserPage extends Component {
   }
 
   render() {
-    console.log( 'in USERRRRRRRRPAGE' )
-    const items = this.props.store.clothing.filter(( item )=>{
-        if( this.state.inputValue == null) {
-          return item
-        } else if ( item.color.toLowerCase().includes(this.state.inputValue.toLowerCase()) ||
-            item.type.toLowerCase().includes(this.state.inputValue.toLowerCase()) || 
-            item.material.toLowerCase().includes(this.state.inputValue.toLowerCase()) ||
-            item.kind.toLowerCase().includes(this.state.inputValue.toLowerCase()) || 
-            item.description.toLowerCase().includes(this.state.inputValue.toLowerCase()) || 
-            item.brand.toLowerCase().includes(this.state.inputValue.toLowerCase())) {
-          return item
-        } 
-      }).map( (item, i ) =>{
-        return (
-          
-          <div className='closetItem' key={ i }>
-            <ul>
-              <Card
-              style={{ margin: 5 }}>
-                  <img src={ item.image_url } 
-                      value={ item.key }
-                      onClick={() => this.onItemClick( item )}>
-                  </img>
-              <Typography variant='subtitle2'>{ item.brand }</Typography>
-              </Card>
-            </ul>
-          </div>
-
-      )
-    })
-
+    console.log( 'USErRRRRRPAGE:', this.props.store )
+    console.log( 'Userpage this.props.store.clothing:', this.props.store.clothing )
+    if ( this.props.store.clothing.length === 0 ) {
+      return (
+      <div className='newUserNotice'>
+        <Typography variant='h5'>Your closet is empty!</Typography>
+        <Typography variant='h6'>Add your first item!</Typography>
+        <LibraryAddIcon style={{ width: 50, height: 50 }} fontSize='large' onClick={ this.addItem }></LibraryAddIcon>
+    </div>
+    )
+    } else { 
     return (
         <div>
-            <div className = 'searchBar'>
-                <SearchBar 
-                    style={{ width: 200, marginLeft: 20 }}
-                    value={ this.state.search }
-                    onChange={( newValue ) => this.setState({ inputValue: newValue })}
-                    onRequestSearch={() => this.filterByInput( this.state.inputValue )}/>
-                    { items.length === 0 || items === null ? (
-                    <div className='newUserNotice'>
-                      <Typography variant='h5'>Your closet is empty!</Typography>
-                      <Typography variant='h6'>Add your first item!</Typography>
-                      <LibraryAddIcon style={{ width: 50, height: 50 }} fontSize='large' onClick={ this.addItem }></LibraryAddIcon>
-                    </div>
-                    ) : 
-                    (<div style={{ textAlign:'center' }}>{ items }</div>) }
-            </div>
-      </div>
+                <Searchish></Searchish>
+        </div>
     );
   }
+}
 }
 
 // this allows us to use <App /> in index.js
